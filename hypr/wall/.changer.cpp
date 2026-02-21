4c6581c89013a22ca2933b7e14fba461cd165488
Brian Yung <byung8298@gmail.com>
2026-02-21 02:25:18 -0600

    Walman still needs mpvpaper support. Added default wallpaper feature.

--- hypr/wall/.changer.cpp
+++ hypr/wall/.changer.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     if(argc == 1) {
-        cout << "Not enough arguments. " << endl;
+        cout << "Not enough arguments. Syntax is walman [add] [use] [default] file" << endl;
         return 1;
     }
     else if(argc == 2) {
@@ -18,8 +18,8 @@ int main(int argc, char* argv[]) {
             return 1;
         }
     }
-    else if(argc == 3) {
-        string s = argv[2];
+    else if(argc > 2) {
+        string s = argv[argc-1];
         int idx = -1;
         for(int i = s.size() - 1; i >= 0; -- i) {
             if(s[i] == '.') {
@@ -34,51 +34,57 @@ int main(int argc, char* argv[]) {
         */ 
         string cd = "cd ~/.config/hypr/wall";
         system(cd.c_str());
-        string op = argv[1];
-        if(op == "add") {
-            //cout << "detected add flag" << endl;
+        for(int i = 1; i < argc - 1; ++ i) {
+            string op = argv[i];
+            if(op == "add") {
+                //cout << "detected add flag" << endl;
 
-            if(idx == -1 || idx == 0 || idx == s.size() - 1) {
-                cout << "Invalid format. " << endl;
-                return 1;
+                if(idx == -1 || idx == 0 || idx == s.size() - 1) {
+                    cout << "Invalid format. " << endl;
+                    return 1;
+                }
+                string extension = s.substr(idx + 1);
+                //swww supported formats: PNG, JPG, JPEG, WEBP, BMP, TGA, PNM
+                if(extension == "png" || extension == "jpg" || extension == "jpeg" || extension == "webp" || extension == "tga" || extension == "pnm") {
+                    string copy = "cp .swww_dummy.conf " + s + ".conf";
+                    string replace = "sed -i 's/YOURIMAGE/" + s + "/' " + s + ".conf";
+                    //cout << copy << endl << replace << endl;
+                    system(copy.c_str());
+                    system(replace.c_str()); 
+                }
+                else {
+                    cout << "Invalid file type." << endl;
+                    return 1;
+                }
             }
-            string extension = s.substr(idx + 1);
-            //swww supported formats: PNG, JPG, JPEG, WEBP, BMP, TGA, PNM
-            if(extension == "png" || extension == "jpg" || extension == "jpeg" || extension == "webp" || extension == "tga" || extension == "pnm") {
-                string copy = "cp .swww_dummy.conf " + s + ".conf";
-                string replace = "sed -i 's/YOURIMAGE/" + s + "/' " + s + ".conf";
-                //cout << copy << endl << replace << endl;
+            else if(op == "use") {
+                string use = "cp " + s + ".conf w.conf";
+                string pipe = "head -n 1 w.conf > .engine_type.temp";
+                system(use.c_str());
+                system(pipe.c_str());
+                ifstream fin(".engine_type.temp");
+                if(!fin) {
+                    cout << "Error reading from temp file." << endl;
+                    return 1;
+                }
+                string type;
+                fin >> type;
+                cout << type << endl;
+                if(type == "#swww") {
+                    string pywal = "wal -i ~/.config/wallpapers/" + s + " >/dev/null 2>&1";
+                    system(pywal.c_str());
+                }
+                string cleanup = "rm .engine_type.temp >/dev/null 2>&1";
+                system(cleanup.c_str());
+            }
+            else if(op == "default") {
+                string copy = "cp " + s + ".conf d.conf";
                 system(copy.c_str());
-                system(replace.c_str()); 
             }
             else {
-                cout << "Invalid file type." << endl;
+                cout << "Invalid operator " << op << endl;
                 return 1;
             }
-        }
-        else if(op == "use") {
-            string use = "cp " + s + ".conf w.conf";
-            string pipe = "head -n 1 w.conf > .engine_type.temp";
-            system(use.c_str());
-            system(pipe.c_str());
-            ifstream fin(".engine_type.temp");
-            if(!fin) {
-                cout << "Error reading from temprorary file." << endl;
-                return 1;
-            }
-            string type;
-            fin >> type;
-            cout << type << endl;
-            if(type == "#swww") {
-                string pywal = "wal -i ~/.config/wallpapers/" + s + " >/dev/null 2>&1";
-                system(pywal.c_str());
-            }
-            string cleanup = "rm engine_type.temp >/dev/null 2>&1";
-            system(cleanup.c_str());
-        }
-        else {
-            cout << "Unknown flag. --help for info" << endl;
-            return 1;
         }
     }
 }
